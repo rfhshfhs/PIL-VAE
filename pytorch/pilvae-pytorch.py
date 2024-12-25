cb7073c1181aa8ee83763a2e6651da80105d1e79
ZiQi_Zhao <46570526+rfhshfhs@users.noreply.github.com>
2024-12-25 15:16:56 +0800

    Update pilvae-pytorch.py
    new pytorch code

--- pytorch/pilvae-pytorch.py
+++ pytorch/pilvae-pytorch.py
@@ -1,284 +1,325 @@
 import numpy as np
 import torch
-import matplotlib
-from torch.distributions import multivariate_normal
-
-matplotlib.use('TkAgg')
+from sklearn.preprocessing import MinMaxScaler
+from scipy.linalg import orth
 import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
 from torchvision import datasets, transforms
-
-# 参数定义
-para = 1
-actFun = 'prelu'  # prelu gau
-batch_size = 16
-l = 1
-epochs = 3
-mean_value = 0
-sig = 1e-2
-latent_dim = 15
-lambda0 = 1e-3
-vae = []
-HiddenO = []
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('/data', train=True, download=True,
-                   transform=transforms.ToTensor()),
-    batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('/data', train=False, transform=transforms.ToTensor()),
-    batch_size=batch_size, shuffle=False)
-
-# 遍历 DataLoader 中的每一个 batch
-target_label = 2  # 筛选的标签值
-num_samples = 6000  # 需要抽取的样本数量
-target_data = []
-for batch_data, batch_label in train_loader:
-    # 筛选和过滤指定标签的数据
-    mask = torch.eq(batch_label, target_label)
-    indices = torch.nonzero(mask).squeeze()
-    target_batch_data = torch.index_select(batch_data, 0, indices)
-
-    # 将每一个 batch 的指定标签的数据添加到结果列表中
-    target_data.append(target_batch_data)
-# 拼接所有的数据
-target_data = torch.cat(target_data, dim=0)
-
-# 如果筛选出的数据量多于需要的数量，随机抽样
-if target_data.size(0) >= num_samples:
-    # 创建随机索引
-    indices = torch.randperm(target_data.size(0))[:num_samples]
-    # 根据索引抽取数据
-    target_data = target_data[indices]
-
-# 确保最后形状符合要求，并处理可能的情况：筛选出的数据不足 num_samples
-actual_num_samples = min(num_samples, target_data.size(0))
-X_train_2d = target_data.reshape(actual_num_samples, -1)
-X_train_2d = (X_train_2d + 1) / 2
-input_size = X_train_2d.shape[1]  # dim of input data
-hidden_size = X_train_2d.shape[0]
-
 
 def ActivationFunc(tempH, ActivationFunction, p):
     if ActivationFunction == 'relu':
-        #         tempH[tempH <= 0] = 0
-        #         tempH[tempH > 0] = tempH
-        #         H = tempH
         H = np.maximum(0, tempH)
     elif ActivationFunction == 'prelu':
-        alpha = 0.02;
-        #         tempH[tempH <= 0] = alpha*tempH;
-        #         H = tempH
+        alpha = 0.02
         H = np.maximum(alpha * tempH, tempH)
-    elif ActivationFunction == 'gelu':  # xσ(1.702x)
+    elif ActivationFunction == 'gelu':
         H = tempH * 1.0 / (1 + np.exp(-p * tempH * 1.702))
-    elif ActivationFunction == 'sigmod':
+    elif ActivationFunction == 'sigmoid':
         H = 1.0 / (1 + np.exp(-tempH))
-    elif ActivationFunction == 'srelu':
-        tempH[tempH <= 0] = 0
-        tempH[tempH > 0] = tempH
-        H = tempH
     elif ActivationFunction == 'sin':
         H = np.sin(tempH)
     elif ActivationFunction == 'tanh':
-        H = np.np.tanh(tempH)
+        H = np.tanh(tempH)
     return H
 
-
-def torchOrth(A):
-    # Perform Singular Value Decomposition
-    u, s, v = torch.linalg.svd(A, full_matrices=False)
-    # Normalize each column to turn them into unit vectors
-    norm_u = u / u.norm(dim=0)
-    return norm_u
-
-
-def PIL0(InputLayer, input_size, hidden_size, l):
-    InputWeight = torch.randn(hidden_size, input_size)
-    if hidden_size >= input_size:
-        InputWeight = torchOrth(InputWeight)
-    else:
-        InputWeight = torchOrth(InputWeight.T).T
-    matrix_rank = torch.linalg.matrix_rank(InputLayer)
-    tempH = torch.mm(InputWeight, InputLayer)
-    H1 = ActivationFunc(tempH, actFun, para)
-    l = l + 1
-    InputLayer = H1
-    hidden_size = InputLayer.shape[1]
-    input_size = InputLayer.shape[0]
-    vae.append(InputWeight)
-    HiddenO.append(H1)
-    return InputLayer, hidden_size, input_size, l
-
-
-def Gn_PIL(InputLayer, input_size, hidden_size, l):
-    InputLayer_pinv = torch.pinverse(InputLayer)
-    noisy = torch.normal(mean_value, sig, size=InputLayer.shape)
-    InputLayer_pinv = InputLayer + noisy
-    tempH = torch.matmul(InputLayer_pinv, InputLayer)
-    H2 = ActivationFunc(tempH, actFun, para)
-    l = l + 1
-    vae.append(InputLayer_pinv)
-    HiddenO.append(H2)
-    return H2, l
-
-
 def ppcamle(data, q):
-    # data: 输入数据，N行d列
-    # q: 潜在空间的维度
     N, d = data.shape
-    mu = torch.mean(data, dim=0)
-    T = data - mu  # Broadcasting automatically expands mu
-    S = T.t().mm(T) / N  # S = T' * T / N
-    D, V = torch.linalg.eig(S)  # Eigenvalue decomposition, 注意：返回的特征值是复数
-    D = torch.real(D)  # 取特征值的实部
-    V = torch.real(V)  # 确保特征向量同样是实数类型
-    sorted_indices = torch.argsort(D, descending=True)  # 降序排序特征值
+    mu = np.mean(data, axis=0)
+    T = data - mu
+    S = T.T.dot(T) / N
+    D, V = np.linalg.eigh(S)
+    sorted_indices = np.argsort(D)[::-1]
     D = D[sorted_indices]
     V = V[:, sorted_indices]
-    sigma = torch.sum(D[q:]) / (d - q)  # 计算σ
-    Uq = V[:, :q]  # 提取前q个特征向量
-    lambda_q = D[:q]  # 提取前q个特征值
-    diag_lambda_q = torch.diag(lambda_q - sigma)  # 构建对角矩阵 lambda_q - sigma*I
-    w = torch.mm(Uq, torch.sqrt(diag_lambda_q))  # w = Uq * sqrt(lambda_q - sigma*I)
-    C = w.mm(w.t()) + sigma * torch.eye(d)  # 计算协方差矩阵 C
-    log_2pi = torch.log(torch.tensor(2.0 * np.pi))
-    sign, log_det_C = torch.linalg.slogdet(C)
-    assert sign > 0  # 确保行列式为正
-    invC_S = torch.linalg.solve(C, S)
-    trace_invC_S = torch.trace(invC_S)
-    L = -N * (d * log_2pi + log_det_C + trace_invC_S / 2.0)
+    sigma = np.sum(D[(q + 1):]) / (d - q)
+    Uq = V[:, :q]
+    lambda_q = D[:q]
+    w = Uq.dot(np.sqrt(np.diag(lambda_q) - sigma * np.identity(q)))
+    C = w.dot(w.T) + sigma * np.identity(d)
+    
+    det_C = np.linalg.det(C)
+    if det_C < 1e-10:
+        det_C = 1e-10
+    L = -N * (d * np.log(2 * np.pi) + np.log(det_C) + np.trace(np.linalg.solve(C, S)) / 2)
     return mu, w, sigma, L
 
+latent_dim = 12
+MAX_EN_LAYER = 2
+mean_value = 0
+sig = 1e-2
+para = 1
+actFun = 'prelu'
+lambda0 = 1e-3
+vae = []
+HiddenO = []
+l = 1
+value_to_find = 6# 查找的索引值
+num_training_samples = 2000 # 训练样本量
 
-def ppca(H2, l, latent_dim=latent_dim):
-    mu, w, sigma, L = ppcamle(H2, latent_dim)
-    # 计算逆矩阵的一部分
-    A = w.T @ w + sigma * torch.eye(latent_dim)
-    # 计算逆矩阵
-    A_inv = torch.inverse(A)
-    mu_expanded = mu.unsqueeze(1).expand_as(H2.T)
-    # 应用公式计算 Z
-    Z = A_inv @ w.T @ (H2.T - mu_expanded)
-    vae.append(w)
-    return Z, l, sigma
 
+transform = transforms.Compose([transforms.ToTensor()])
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
-def Zrec(Z, H2, l, lambda0=lambda0):
-    ZZT = torch.matmul(Z, Z.T)
-    # 计算 H2 和 Z.T 的点积
-    HZT = torch.matmul(H2, Z.T)
-    # 创建一个与 ZZT 相同维度的单位矩阵，并乘以lambda0
-    I = lambda0 * torch.eye(latent_dim, dtype=ZZT.dtype, device=ZZT.device)
-    # 计算括号内的矩阵和它的伪逆
-    pinv = torch.pinverse(ZZT + I)
-    # 最后计算 OutputWeight
-    OutputWeight = torch.matmul(HZT, pinv)
-    tempH = torch.matmul(OutputWeight, Z)
+train_images = train_dataset.data.numpy()
+train_labels = train_dataset.targets.numpy()
+test_images = test_dataset.data.numpy()
+test_labels = test_dataset.targets.numpy()
+print("合并前的数据集形状:", train_images.shape, train_images.shape)
+
+all_images = np.concatenate((train_images, test_images), axis=0)
+all_labels = np.concatenate((train_labels, test_labels), axis=0)
+print("合并后的数据集形状:", all_images.shape, all_labels.shape)
+train_labels
+type(train_labels)
+# 使用numpy.min()函数找到最小值
+min_value = np.min(train_labels)
+# 使用numpy.max()函数找到最大值
+max_value = np.max(train_labels)
+print("最小值:", min_value, "最大值:", max_value)
+indices = np.where(train_labels == value_to_find)
+# 打印索引元组
+print(f"值为{value_to_find}的索引:", indices)
+type(train_images)
+# 使用切片操作选中指定序号的图像
+selected_images = train_images[indices]
+
+# 打印结果
+print("选中的图像形状:", selected_images.shape)
+len(indices[0])
+# 使用NumPy生成随机整数序列
+ridx = np.random.randint(0, selected_images.shape[1] - 1, num_training_samples)
+X_train = selected_images[ridx]
+
+# 将后两维合并成一个二维数组，保持第一维不变
+X_train_2d = X_train.reshape(X_train.shape[0], -1)  # 每行表示一个观察或样本，每列表示一个特征或变量
+# 打印结果
+print("原始三维数组形状:", X_train.shape)
+print("合并后的二维数组形状:", X_train_2d.shape)
+rank_data = np.linalg.matrix_rank(X_train_2d)
+print("输入的秩：", rank_data)
+input_dim = X_train_2d.shape[1]  # dim of input data
+hidden_dim = X_train_2d.shape[0]  # Gn-PIL dim of H
+
+# Normalization
+scaler = MinMaxScaler(feature_range=(0, 1))  # 映射到 0-1 之间
+X_train_2d = scaler.fit_transform(X_train_2d)
+InputLayer = X_train_2d.T
+print("InputLayer's shape:", InputLayer.shape)
+
+while l <= MAX_EN_LAYER:
+    # %%%%%%%%%% Encoder %%%%%%%%%%
+    # %%%%%% 1st layer: PIL0
+    # 使用NumPy创建具有随机值的二维数组
+    InputWeight = np.random.randn(hidden_dim, input_dim)
+    if hidden_dim >= input_dim:
+        InputWeight = orth(InputWeight)
+    else:
+        InputWeight = orth(InputWeight.T).T
+    # Compute the rank of the matrix InputLayer
+    print(InputWeight.shape)
+    print(InputLayer.shape)
+    matrix_rank = np.linalg.matrix_rank(InputLayer)
+    print("rank of InputLayer：", matrix_rank)
+
+    print("inputweight:", InputWeight)
+    tempH = InputWeight.dot(InputLayer)
+    H1 = ActivationFunc(tempH, actFun, para)
+    vae.append(InputWeight)  # vae{l}.WI = InputWeight
+    HiddenO.append(H1)
     l = l + 1
-    vae.append(OutputWeight)
-    return tempH, l
+
+    InputLayer = H1
+    hidden_dim = InputLayer.shape[1]
+    input_dim = InputLayer.shape[0]
+    print("InputLayer shape:", InputLayer.shape)
+InputLayer_pinv = np.linalg.pinv(InputLayer)
+# Compute the rank of the matrix InputLayer
+matrix_rank = np.linalg.matrix_rank(InputLayer_pinv)
+print("rank of InputLayer：", matrix_rank)
+
+# 生成具有指定均值和标准差的随机数，大小与 InputLayer_pinv 相同
+random_noise = np.random.normal(mean_value, sig, size=InputLayer_pinv.shape)
+
+# 将随机噪声添加到 InputLayer_pinv 中
+InputLayer_pinv = InputLayer_pinv + random_noise
+
+# Compute the rank of the matrix InputLayer
+matrix_rank = np.linalg.matrix_rank(InputLayer_pinv)
+print("rank of matrix after GnPIL：", matrix_rank)
+
+vae.append(InputLayer_pinv)
+tempH = InputLayer_pinv.dot(InputLayer)
+H2 = ActivationFunc(tempH, actFun, para)
+HiddenO.append(H2)
+l = l + 1
+Y = H2.T
+H2.shape
 
 
-def H2rec(tempH, l, dl, lambda0=lambda0, hidden_size=hidden_size):
-    tempH_T = tempH.T
-    tempHHT = torch.matmul(tempH, tempH.T)
-    eye_matrix = lambda0 * torch.eye(hidden_size)
-    step1 = torch.matmul(HiddenO[dl - 1], tempH_T)
-    # numpy.linalg.pinv(tempHHT + eye_matrix)
-    # 先添加单位矩阵到 tempHHT:
-    regularized = tempHHT + eye_matrix  # 这里假设 eye_matrix 已经是像 torch.eye 生成的单位矩阵
-    # 计算伪逆
-    step2 = torch.pinverse(regularized)
-    # 现在进行最终的点乘操作得到 OutputWeight:
-    OutputWeight = torch.matmul(step1, step2)
-    tempH = torch.matmul(OutputWeight, tempH)
-    l = l + 1
-    dl = dl - 1
-    return tempH, l, dl
 
 
-def H1rec(tempH, X_train_2d, hidden_size=hidden_size, lambda0=lambda0):
-    tempHHT = torch.matmul(tempH, tempH.T)
-    lambda_eye = lambda0 * torch.eye(hidden_size)
-    X_train_2d_rec_H1_T = torch.matmul(X_train_2d.T, tempH.T)
-    # 计算 rec_H1H1T 和 lambda_eye 的和
-    rec_H1H1T_lambda_eye = tempHHT + lambda_eye
-    # 接着计算伪逆
-    rec_H1H1T_lambda_eye_pinv = torch.pinverse(rec_H1H1T_lambda_eye)
-    # 最后计算 OutputWeight
-    OutputWeight = torch.matmul(X_train_2d_rec_H1_T, rec_H1H1T_lambda_eye_pinv)
-    vae.append(OutputWeight)
-    tempH = torch.matmul(OutputWeight, tempH)
-    return tempH
-
-
-X_train_3d = X_train_2d
-X_train_2d = X_train_2d.T
-print(X_train_3d.shape)
-print(X_train_2d.shape)
-for epoch in range(epochs):
-    X_train_2d, input_size, hidden_size, l = PIL0(X_train_2d, input_size, hidden_size, l)
-    print(X_train_2d.shape)
-    print(0)
-
-H2, l = Gn_PIL(X_train_2d, input_size, hidden_size, l)
-print(H2.shape)
-H2 = H2.T
-Z, l, sigma = ppca(H2, l)
-print(Z.shape)
-rec_H2, l = Zrec(Z, H2, l)
-print(rec_H2.shape)
+mu, w, sigma, L = ppcamle(Y, latent_dim)
+print("sigma:", sigma)
+# 计算 Z  Z 是一个 (latent_dim, num_trainingSamples) 的矩阵
+Z = np.linalg.solve(w.T.dot(w) + sigma * np.identity(latent_dim), w.T).dot(
+    Y - np.tile(mu.T, (num_training_samples, 1)).T)
+print("shape of Z:", Z.shape)
+vae.append(w)
+l = l + 1
+# Decoder
+# 1st layer: Using Z to reconstruct H2
+ZZT = Z.dot(Z.T)
+# OutputWeight = H2.dot(Z.T) / (ZZT + lambda0 * np.eye(latent_dim))
+OutputWeight = H2.dot(Z.T).dot(np.linalg.pinv(ZZT + lambda0 * np.eye(latent_dim)))
+print("layer of rec H2:", l)
+vae.append(OutputWeight)
+tempH = OutputWeight.dot(Z)
+rec_H2 = tempH
+l = l + 1
 print(l)
-dl = l - 3
-while dl>0:
-    rec_H1, l, dl = H2rec(rec_H2, l, dl)
-    print(rec_H1.shape)
-    print(1)
-rec_X = H1rec(rec_H1, X_train_3d)
-print(rec_X.T.shape)
-# print(rec_X.T)
-rec_X_np = rec_X.T[2].detach().numpy()
-# 用reshape方法改变数组形状，而不是view
-rec_X_np_reshaped = rec_X_np.reshape(28, 28)
-# 显示图像，abs确保所有数值是非负的，cmap指定色彩映射
-plt.imshow(np.abs(rec_X_np_reshaped), cmap='gray')
-plt.show()  # 显示图像
+# 2nd layer: Using H2 to reconstruct H1
+dl = l - 4
+while dl > 0:
+    # 计算 OutputWeight
+    tempH_transpose = tempH.T
+    tempHHT = tempH.dot(tempH_transpose)
+    eye_matrix = lambda0 * np.eye(hidden_dim)
+    #     OutputWeight = HiddenO[dl - 1].dot(tempH_transpose) / (tempHHT + eye_matrix)
+    OutputWeight = (HiddenO[dl - 1].dot(tempH_transpose)).dot(np.linalg.pinv(tempHHT + eye_matrix))
+    vae.append(OutputWeight)  # 5
+
+    # 更新 tempH
+    tempH = OutputWeight.dot(tempH)
+
+    # 更新 rec_H1 (如果需要)
+    rec_H1 = tempH  # 此行可以根据需要添加
+
+    l += 1
+    dl -= 1
+# 3rd layer:  Using H1 to reconstruct X
+rec_H1H1T = rec_H1.dot(rec_H1.T)
+lambda_eye = lambda0 * np.eye(hidden_dim)
+
+# 计算 OutputWeight
+# OutputWeight = X_train_2d.dot(rec_H1.T) / (rec_H1H1T + lambda_eye)
+OutputWeight = (X_train_2d.T.dot(rec_H1.T)).dot(np.linalg.pinv(rec_H1H1T + lambda_eye))
+vae.append(OutputWeight)  # 6
+
+# 计算 tempX
+tempX = OutputWeight.dot(rec_H1)
+
+# 更新 rec_X
+rec_X = tempX
+rec_X.T.shape
+rec_X.T[0]
+type(rec_X.T[0])
 
 
-# # Generating new sample
-# num_samples = 1000
-# latent_samples = np.random.randn(latent_dim, num_samples)
-# # Generate the covariance matrix
-# print(sigma)
-# covariance = sigma * np.eye(hidden_size)
-# # Generate noise from the Gaussian distribution
-# # ns = mvnrnd(zeros(num_samples, hidden_dim), covariance, num_samples);
-# ns = multivariate_normal.rvs(mean=np.zeros(hidden_size), cov=covariance, size=num_samples)
-# print("ns's shape:", ns.shape)
-# # Generate new samples
-# hidden_samples1 = w.dot(latent_samples) + np.tile(mu.T, (num_samples, 1)).T
-# l = 4 + MAX_EN_LAYER
-# generated_samples2 = vae[l - 1].dot(hidden_samples1)
-# temprecH = generated_samples2
-# while l < 4 + MAX_EN_LAYER * 2:
-#     l = l + 1
-#     temprecH = vae[l - 1].dot(temprecH)
-#
-# generated_X = temprecH
-# print("generated_X:", generated_X.shape)
-# # 使用imshow函数将数组绘制成图像
-# plt.imshow(np.abs(generated_X.T[0].reshape((28, 28))), cmap='gray')  # cmap参数指定了使用灰度颜色映射
-#
-# # 显示图像
-# plt.show()
-# # 设置图形大小
-# plt.figure(figsize=(8, 8))
-# #     遍历每张图片并将其添加到图形子集中
-# for i in range(100):
-#     plt.subplot(10, 10, i + 1)
-#     plt.imshow(np.abs(generated_X.T[i].reshape((28, 28))), cmap='gray')  # 假设这是灰度图像
-#     plt.axis('off')  # 关闭坐标轴
-#
-# # 显示图形
-# plt.show()
+# 使用imshow函数将数组绘制成图像
+plt.imshow(np.abs(rec_X.T[0].reshape((28, 28))), cmap='gray')  # cmap参数指定了使用灰度颜色映射
+
+# 显示图像
+plt.show()
+num_samples_test = 1000
+# value_to_find = 2
+# 使用numpy.where()查找值为2的索引
+indices = np.where(test_labels == value_to_find)
+
+# 打印索引元组
+print("值为2的索引:", indices)
+# 使用切片操作选中指定序号的图像
+selected_TestImages = test_images[indices]
+
+# 打印结果
+print("选中的图像形状:", selected_TestImages.shape)
+len(indices[0])
+# 使用NumPy生成随机整数序列
+ridx = np.random.randint(0, selected_TestImages.shape[1] - 1, num_samples_test)
+X_test = selected_TestImages[ridx]
+
+# 将后两维合并成一个二维数组，保持第一维不变
+X_test_2d = X_test.reshape(X_test.shape[0], -1)  # 每行表示一个观察或样本，每列表示一个特征或变量
+
+# 打印结果
+print("原始三维数组形状:", X_test.shape)
+print("合并后的二维数组形状:", X_test_2d.shape)
+X_test_2d = scaler.fit_transform(X_test_2d)
+
+InputLayer = X_test_2d.T
+print("InputLayer's shape:", InputLayer.shape)
+l = 1
+while l <= MAX_EN_LAYER:
+    tempH_test1 = np.dot(vae[l - 1], InputLayer)
+    H_test1 = ActivationFunc(tempH_test1, actFun, para)
+    InputLayer = H_test1
+    l += 1
+tempH_test2 = np.dot(vae[l - 1], InputLayer)
+H_test2 = ActivationFunc(tempH_test2, actFun, para)
+InputLayer = H_test2
+l += 1
+print(l)
+print("mu's shape:", mu.T.shape)
+InputLayer_mu = InputLayer - np.tile(mu, (num_samples_test, 1)).T
+Z_test = (vae[l - 1].T.dot(vae[l - 1]) + sigma * np.eye(latent_dim)).dot(np.linalg.pinv(vae[l - 1])).dot(InputLayer_mu)
+l = l + 1
+print(l)
+H_rec_test2 = np.dot(w, Z_test) + np.tile(mu.T, (num_samples_test, 1)).T
+l = l + 1
+print(l)
+dl = l - 4
+while dl > 0:
+    H_rec_test1 = vae[l - 1].dot(H_rec_test2)
+
+    l += 1
+    dl -= 1
+X_rec_test = vae[l - 1].dot(H_rec_test1)
+print("RecTest's shape", X_rec_test.shape)
+# 设置图形大小
+plt.figure(figsize=(8, 8))
+#     遍历每张图片并将其添加到图形子集中
+for i in range(100):
+    plt.subplot(10, 10, i + 1)
+    plt.imshow(np.abs(X_rec_test.T[i].reshape((28, 28))), cmap='gray')  # 假设这是灰度图像
+    plt.axis('off')  # 关闭坐标轴
+
+# 显示图形
+plt.show()
 
 
+# Generating new sample
+num_samples = 1000
+latent_samples = np.random.randn(latent_dim, num_samples)
+# Generate the covariance matrix
+print(sigma)
+covariance = sigma * np.eye(hidden_dim)
+# Generate noise from the Gaussian distribution
+# ns = mvnrnd(zeros(num_samples, hidden_dim), covariance, num_samples);
+ns = multivariate_normal.rvs(mean=np.zeros(hidden_dim), cov=covariance, size=num_samples)
+print("ns's shape:", ns.shape)
+# Generate new samples
+hidden_samples1 = w.dot(latent_samples) + np.tile(mu.T, (num_samples, 1)).T
+l = 4 + MAX_EN_LAYER
+generated_samples2 = vae[l - 1].dot(hidden_samples1)
+temprecH = generated_samples2
+while l < 4 + MAX_EN_LAYER * 2:
+    l = l + 1
+    temprecH = vae[l - 1].dot(temprecH)
+
+generated_X = temprecH
+print("generated_X:", generated_X.shape)
+# 使用imshow函数将数组绘制成图像
+plt.imshow(np.abs(generated_X.T[0].reshape((28, 28))), cmap='gray')  # cmap参数指定了使用灰度颜色映射
+
+# 显示图像
+plt.show()
+# 设置图形大小
+plt.figure(figsize=(8, 8))
+#     遍历每张图片并将其添加到图形子集中
+for i in range(100):
+    plt.subplot(10, 10, i + 1)
+    plt.imshow(np.abs(generated_X.T[i].reshape((28, 28))), cmap='gray')  # 假设这是灰度图像
+    plt.axis('off')  # 关闭坐标轴
+
+# 显示图形
+plt.show()
